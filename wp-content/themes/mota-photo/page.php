c6750b2ff04cad31a1bfe258a7b69b92cd0459b8
Carole Ibarlucia <carole.ibarlucia@sfr.fr>
2024-07-16 21:42:34 +0200

    maj

---
+++ wp-content/themes/mota-photo/page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+  get_header(); ?>
+
+ <main>
+	 <?php
+	 if ( have_posts() ) : 
+		 while ( have_posts() ) : the_post(); ?>
+			 <h1><?php the_title(); ?></h1>
+			 <div><?php the_content(); ?></div>
+		 <?php endwhile;
+	 else : ?>
+		 <p>Aucun contenu trouvé</p>
+	 <?php endif; ?>
+ </main>
+ 
+ <?php get_footer(); ?>
+ 
